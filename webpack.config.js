7e71cee02f46c448adee0cde5a43a524b10489f0
ivveta <31349913+ivveta@users.noreply.github.com>
2022-12-19 23:37:56 +0200

    add minimizers (#5)
    * minimize files
    
    * remove extra changes
    
    Co-authored-by: s.ivanova <s.ivanova@adsterra.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   // корневая папка, где webpack будет искать файлы, все пути указывать от нее
@@ -29,38 +34,18 @@ module.exports = {
     },
   },
   optimization: {
-    // разбить output на чанки
+    // разбивает output на чанки
     splitChunks: {
       chunks: 'all',
     },
+    // минимизация JS c помощью плагина Terser
+    // минимизация CSS c помощью плагина CssMinimizerPlugin
+    // минимизация по-умолчанию в webpack работает только для prod
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
   target: 'web',
   devServer: {
-    // включить hot reload
-    hot: true,
-    static: {
-      directory: path.join(__dirname, 'src'),
-    },
-  },
-  resolve: {
-    // какие расширения понимать по-умолчанию, чтобы не писать в import
-    extensions: ['.js', '.json', '.png'],
-    // алиасы в import
-    alias: {
-      '@models': path.resolve(__dirname, 'src/models'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-    },
-  },
-  optimization: {
-    // разбить output на чанки
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-  target: 'web',
-  devServer: {
-    // включить hot reload
+    // включает hot reload
     hot: true,
     static: {
       directory: path.join(__dirname, 'src'),
@@ -70,8 +55,12 @@ module.exports = {
     new HTMLWebpackPlugin({
       // шаблон для html, скрипты webpack подключит сам
       template: './index.html',
+      minify: {
+        // минимизирует html при prod сборке
+        collapseWhitespace: isProd,
+      },
     }),
-    //копирует файлы из src в dist
+    // копирует файлы из src в dist
     new CopyWebpackPlugin({
       patterns: [
         {
@@ -80,6 +69,9 @@ module.exports = {
         },
       ],
     }),
+    // выносит css в отдельный файл
+    // Hot Module Reloading HMR для CSS автоматически поддерживается в webpack 5.
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
   ],
   // лоадеры позволяют webpack работать с файлами, отличными от js
   module: {
@@ -87,9 +79,10 @@ module.exports = {
       {
         test: /\.css$/,
         // webpack пропускает справа налево: сначала css-loader, потом style-loader
-        // css-loader позволяет делать imports css in js
+        // MiniCssExtractPlugin.loader выносит css в отдельный файл
+        // css-loader позволяет делать imports css in js (не используется в проекте)
         // style-loader добавляет стили в <head> html
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
